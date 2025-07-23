01896439aeef5889f1401c5f58d3cc8b906ead74
Krzysztof Cieplucha <krisiasty@gmail.com>
2025-07-23 21:49:30 +0200

    add main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+)
+
+var (
+	// version stromg will be set by the build system
+	version string = "dev"
+)
+
+func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "Enable debug mode")
+	flag.Parse()
+
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/krisiasty/iputils",
+	}
+
+	err := providerserver.Serve(context.Background(), NewIPUtilsProvider(version), opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
